3d95d35ea1ab31583c16f54bcc30c77d45931d7e
Link4Electronics <link4electronics@gmail.com>
2026-01-26 03:25:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,17 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q powermanga | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q sdlpop | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/powermanga.png
-export DESKTOP=/usr/share/applications/powermanga.desktop
+export ICON=/opt/sdlpop/data/icon.png
+export DESKTOP=/usr/share/applications/sdlpop.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/powermanga
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/share/games/powermanga' >> ./AppDir/.env
+quick-sharun /usr/bin/prince
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/share/opt/sdlpop' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
